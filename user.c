2528a5f3f5228dddb0f8436cf13823328f51e073
MaksFardzinov <115629893+MaksFardzinov@users.noreply.github.com>
2024-01-20 01:42:59 +0300

    Update user.c

--- user.c
+++ user.c
@@ -7,33 +7,28 @@
 #define MAX_BUFFER_SIZE 1024
 
 int main() {
-    int fd;
+    int cpuFile;
     ssize_t bytes_read;
     char buffer[MAX_BUFFER_SIZE];
 
-    // Открываем файл модуля
-    fd = open("/proc/my_module", O_RDONLY);
-    if (fd == -1) {
+    cpuFile = open("/proc/my_module", O_RDONLY);
+    if (cpuFile == -1) {
         perror("Error opening file");
         return EXIT_FAILURE;
     }
 
-    // Читаем данные из файла
-    bytes_read = read(fd, buffer, MAX_BUFFER_SIZE - 1);
+    bytes_read = read(cpuFile, buffer, MAX_BUFFER_SIZE - 1);
     if (bytes_read == -1) {
         perror("Error reading file");
-        close(fd);
+        close(cpuFile);
         return EXIT_FAILURE;
     }
 
-    // Добавляем символ окончания строки
     buffer[bytes_read] = '\0';
 
-    // Выводим считанные данные
     printf("Data read from module:\n%s\n", buffer);
 
-    // Закрываем файл
-    close(fd);
+    close(cpuFile);
 
     return EXIT_SUCCESS;
 }
